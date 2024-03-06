b46270b9149a8ad40656870c7c82e8925f208a87
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 15:06:38 +0530

    Update weatherapp.js

--- pages/weatherapp.js
+++ pages/weatherapp.js
@@ -1,1 +1,24 @@
+import React from "react";
+import { useRouter } from "next/router";
+const WeatherApplication = (props)=>{
+  const weatherdata = props.weatherdata;
+  // const router = useRouter()
+  // const { weatherdata } = router.query
+    return(
+    
+          weatherdata.slice(0,31).map((val)=>{
+            console.log(val.time);
+            console.log(val.data.instant.details.air_temperature);
+
+            return(<tr>
+              <td>{ new Date(val.time).toLocaleString()}</td>
+              <td>{val.data.instant.details.air_temperature.toFixed(1)}</td>
+              <td>{val.data.next_1_hours.summary.symbol_code}</td>
+              
+          </tr>)
+          })
+
+    );
+}
+export default WeatherApplication;
 
